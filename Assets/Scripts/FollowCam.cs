0ca59dd09df8342e1936b0cab850003304ed0c6e
Andrew Richman <trickingegg@gmail.com>
2021-09-19 17:52:03 +0300

    Minor code improvements

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
+    private Vector3 _velocity = Vector3.zero;
+
     public Transform target;
     public float smoothTime = 0.2f;
 
-    private Vector3 _velocity = Vector3.zero;
-
-    void LateUpdate()
+    private void LateUpdate()
     {
-        Vector3 targetPosition = new Vector3(
-            target.position.x, target.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
     }
-
 }
--- Assets/Scripts/PlatformerPlayer.cs
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -2,25 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class PlatformerPlayer : MonoBehaviour
 {
-    public float speed = 400.0f;
-    public float jumpForce = 12.0f;
     private Rigidbody2D _body;
     private Animator _anim;
     private BoxCollider2D _box;
 
-    void Start()
+    public float speed = 1500.0f;
+    public float jumpForce = 12.0f;
+
+    private void Start()
     {
         _body = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
         _box = GetComponent<BoxCollider2D>();
     }
 
-    
-    void Update()
+    private void Update()
     {
-        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        var deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         Vector2 movement = new Vector2(deltaX, _body.velocity.y);
         _body.velocity = movement;
 
@@ -32,43 +35,31 @@ public class PlatformerPlayer : MonoBehaviour
 
         bool grounded = false;
         if (hit != null)
-        {
             grounded = true;
-        }
+
         _body.gravityScale = grounded && deltaX == 0 ? 0 : 1; //stop when not moving and grounded
+
         if (grounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); //adding force when pressing space
-        }
+            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
         MovingPlatform platform = null;
         if (hit != null)
-        {
             platform = hit.GetComponent<MovingPlatform>();
-        }
-        if (platform != null)
-        {
-            transform.parent = platform.transform;
-        }
-        else
-        {
-            transform.parent = null;
-        }
 
-        _anim.SetFloat("speed", Mathf.Abs(deltaX)); //making speed positive if Velocity negative
+        if (platform != null)
+            transform.parent = platform.transform;
+
+        else
+            transform.parent = null;
+
+        _anim.SetFloat("speed", Mathf.Abs(deltaX)); //speed positive if Velocity negative
 
         Vector3 pScale = Vector3.one;
         if (platform != null)
-        {
             pScale = platform.transform.localScale;
-        }
+
         if (deltaX != 0)
-        {
-            transform.localScale = new Vector3(Mathf.Sign(deltaX) / pScale.x, 1 / pScale.y, 1);
-        }
-        //if (!Mathf.Approximately(deltaX, 0)) //float numbers dont always match so getting them approxmately
-        //{
-        //    transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
-        //}
+            transform.localScale = new Vector3(Mathf.Sign(deltaX)*0.3f / pScale.x, 0.3f / pScale.y, 1);
+
     }
 }
